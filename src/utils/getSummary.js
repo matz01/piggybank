7f350855da1aeb821423c148a0d6cbbab0870494
matteo <matteo.villa@chili.com>
2022-05-07 10:02:13 +0200

    style

--- src/utils/getSummary.js
+++ src/utils/getSummary.js
@@ -15,8 +15,8 @@ export const getSummary = async (month, budget, callback) => {
 
 			const res = budget.map(item => {
 				const allSpent = response?.data || []
-				const spent = allSpent.find(o => o.id === item.id)?.total || 0
-				const max = item[`m${month}`]
+				const spent = parseInt(allSpent.find(o => o.id === item.id)?.total || 0)
+				const max = parseInt(item[`m${month}`])
 				return {...item, spent, max}
 			})
 			callback(res);
